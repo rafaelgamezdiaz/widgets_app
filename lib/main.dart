fa7df6a1e663c3612f46f37c26313caa7b950e29
Rafael Gamez Diaz <rafaelgamezdiaz@gmail.com>
2025-03-09 15:24:42 -0400

    correccion posicion presentation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:widgets_app/config/presentation/screens/home/home_screen.dart';
+import 'package:widgets_app/presentation/screens/home/home_screen.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
 
 void main() {
