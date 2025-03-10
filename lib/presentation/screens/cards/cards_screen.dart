92645dab432718191d91e11052e253454324b865
Rafael Gamez Diaz <rafaelgamezdiaz@gmail.com>
2025-03-10 19:01:27 -0400

    configuring cards

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
+const cards = <Map<String, dynamic>>[
+  {'elevation': 0.0, 'label': 'Elevatio 0'},
+  {'elevation': 1.0, 'label': 'Elevatio 1'},
+  {'elevation': 2.0, 'label': 'Elevatio 2'},
+  {'elevation': 3.0, 'label': 'Elevatio 3'},
+  {'elevation': 4.0, 'label': 'Elevatio 4'},
+  {'elevation': 5.0, 'label': 'Elevatio 5'},
+];
+
 class CardsScreen extends StatelessWidget {
   static const String name = 'cards_screen';
   const CardsScreen({super.key});
@@ -8,7 +17,57 @@ class CardsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Cards Screen')),
-      body: const Placeholder(),
+      body: _CardsView(),
+    );
+  }
+}
+
+class _CardsView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ...cards.map(
+            (card) =>
+                _CardType1(elevation: card['elevation'], label: card['label']),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _CardType1 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType1({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Card(
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                onPressed: () => {},
+                icon: const Icon(Icons.arrow_right_alt_outlined),
+                style: ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll(colors.tertiary),
+                  foregroundColor: WidgetStatePropertyAll(Colors.white),
+                ),
+              ),
+            ),
+            Align(alignment: Alignment.bottomLeft, child: Text(label)),
+          ],
+        ),
+      ),
     );
   }
 }
